b8995d3e6349887ca7fb7613d4ea879374ae2f94
Krishnagupta20 <krishnagupta5672@gmail.com>
2023-06-10 14:41:36 +0530

    second commit

--- Photo/photo/api/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import Public
-
-class PublicSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Public
-        fields=("name","picture")
